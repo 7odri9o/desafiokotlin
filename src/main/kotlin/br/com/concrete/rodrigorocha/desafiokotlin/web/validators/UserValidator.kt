19f228b8a068df65859074d237c6137835af91ab
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-02-05 15:51:06 -0200

    Adicionando validações no request

--- src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/validators/UserValidator.kt
+++ src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/validators/UserValidator.kt
@@ -7,9 +7,14 @@ import io.javalin.Context
 class UserValidator {
 
     fun validateUserRequest(ctx: Context): UserRequest {
+
         return ctx.validatedBody<UserRequest>()
-            .check({ !it.email.isNullOrBlank()}, "O Campo Email não pode estar vazio")
-            .check({ it.email.isEmailValid()}, "Formato de Email Inválido")
+            .check({ !it.email.isNullOrBlank()}, "O Campo Email deve ser preenchido")
+            .check({ it.email.isEmailValid()},"Formato de Email Inválido")
+            .check({ it.name.isNullOrBlank()}, "O Campo Name deve ser preenchido")
+            .check({ it.password.isNullOrBlank()}, "O Campo Password deve ser preenchido")
+            .check({ it.phones!!.any { phone ->  phone.ddd.isNullOrBlank() } }, "DDD Inválido")
+            .check({ it.phones!!.any { phone ->  phone.number.isNullOrBlank() } }, "Número de Telefone Inválido")
             .getOrThrow()
     }
 }

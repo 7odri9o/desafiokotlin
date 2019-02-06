625ad41c522ac1ec44cbbfc3b3254b3a138fe199
Almir Jr <almirjr.87@gmail.com>
2019-02-06 17:14:37 -0300

    Adiciona tipo genérico no método post da classe HttpUtil

--- src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/handlers/ErrorExceptionMapping.kt
+++ src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/handlers/ErrorExceptionMapping.kt
@@ -1,18 +1,17 @@
 package br.com.concrete.rodrigorocha.desafiokotlin.web.handlers
 
-import br.com.concrete.rodrigorocha.desafiokotlin.web.dto.UserResponse
 import io.javalin.BadRequestResponse
 import io.javalin.Javalin
 import org.eclipse.jetty.http.HttpStatus
 
-//internal data class ErrorResponse(val mensagem: String?)
+internal data class ErrorResponse(val message: String?)
 
 object ErrorExceptionMapping {
 
     fun register(app: Javalin) {
 
         app.exception(BadRequestResponse::class.java) { e, ctx ->
-            val error = UserResponse(message = e.message)
+            val error = ErrorResponse(message = e.message)
             ctx.json(error).status(HttpStatus.BAD_REQUEST_400)
         }
     }

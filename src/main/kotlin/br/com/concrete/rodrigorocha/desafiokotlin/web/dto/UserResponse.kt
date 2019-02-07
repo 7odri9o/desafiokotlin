bbb04c578f8211148a6444f20c4acf8f65d8f8a8
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-02-07 13:16:47 -0200

    Ajustes no DTO

--- src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/dto/UserResponse.kt
+++ src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/dto/UserResponse.kt
@@ -1,18 +1,14 @@
 package br.com.concrete.rodrigorocha.desafiokotlin.web.dto
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.util.*
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class UserResponse(
-
-    @JsonIgnore
-    val id: Long? = null,
-    val created: Date? = null,
-    val modified: Date? = null,
-    val last_login: Date? = null,
-    val token: String? = null,
-    val name: String? = null,
-    val email: String? = null,
-    val message: String? = null)
+    val id: Long,
+    val created: Date,
+    val modified: Date,
+    val last_login: Date,
+    val token: String,
+    val name: String,
+    val email: String)

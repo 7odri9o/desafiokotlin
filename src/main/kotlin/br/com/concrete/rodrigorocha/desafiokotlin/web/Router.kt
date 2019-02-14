3752d0be55c4b3a3437fe9a34bc91a637e32aae6
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-02-14 13:34:21 -0200

    Login Fake no Controller

--- src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/Router.kt
+++ src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/Router.kt
@@ -1,5 +1,6 @@
 package br.com.concrete.rodrigorocha.desafiokotlin.web
 
+import br.com.concrete.rodrigorocha.desafiokotlin.web.controllers.LoginController
 import br.com.concrete.rodrigorocha.desafiokotlin.web.controllers.UserController
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.path
@@ -7,12 +8,19 @@ import io.javalin.apibuilder.ApiBuilder.post
 import org.koin.standalone.KoinComponent
 
 class Router(
-    private val userController: UserController) : KoinComponent {
+    private val userController: UserController,
+    private val loginController: LoginController) : KoinComponent {
 
     fun register(app: Javalin) {
         app.routes {
             path("users") {
                 post(userController::register)
+            }
+        }
+
+        app.routes {
+            path("login") {
+                post(loginController::login)
             }
         }
     }

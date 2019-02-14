7f082e7a0c974deb4b98a903c4573f2e2cf4ee92
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-02-14 12:47:43 -0200

    Adicionando Tratamento para 404

--- src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/handlers/ErrorExceptionMapping.kt
+++ src/main/kotlin/br/com/concrete/rodrigorocha/desafiokotlin/web/handlers/ErrorExceptionMapping.kt
@@ -2,6 +2,7 @@ package br.com.concrete.rodrigorocha.desafiokotlin.web.handlers
 
 import io.javalin.BadRequestResponse
 import io.javalin.Javalin
+import io.javalin.NotFoundResponse
 import org.eclipse.jetty.http.HttpStatus
 
 internal data class ErrorResponse(val message: String?)
@@ -13,6 +14,11 @@ object ErrorExceptionMapping {
         app.exception(BadRequestResponse::class.java) { e, ctx ->
             val error = ErrorResponse(message = e.message)
             ctx.json(error).status(HttpStatus.BAD_REQUEST_400)
+        }
+
+        app.exception(NotFoundResponse::class.java) { _, ctx ->
+            val error = ErrorResponse(message = "Endpoint não encontrado")
+            ctx.json(error).status(HttpStatus.NOT_FOUND_404)
         }
     }
 }
